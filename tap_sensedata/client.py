7e2a19db8dc17aea67869b166f2d262e65553c25
marcos314 <marcoswesley314@gmail.com>
2023-05-09 11:24:29 -0300

    feat(tap_sensedata): Refactor of sending parameters used in the query to return data

--- tap_sensedata/client.py
+++ tap_sensedata/client.py
@@ -1,11 +1,10 @@
+from datetime import date, timedelta
+from typing import Any, Dict, Iterable, Optional
+
 import requests
-
-from typing import Any, Dict, Optional, Iterable
-
-from singer_sdk.helpers.jsonpath import extract_jsonpath
-
-from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import BearerTokenAuthenticator
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
 
 
 class sensedataStream(RESTStream):
@@ -14,13 +13,13 @@ class sensedataStream(RESTStream):
     url_base = "https://api.sensedata.io/v2"
     next_page_token_jsonpath = "$.next_page"
     query_search = {}
+    days_to_decrease_config_name = ""
 
     @property
     def authenticator(self) -> BearerTokenAuthenticator:
         """Return a new authenticator object."""
         return BearerTokenAuthenticator.create_for_stream(
-            self,
-            token=self.config.get("auth_token")
+            self, token=self.config.get("auth_token")
         )
 
     @property
@@ -45,10 +44,31 @@ class sensedataStream(RESTStream):
         self, context: Optional[dict], next_page_token: Optional[Any]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
-        params: dict = self.query_search
+        params: dict = self._prepare_query_search()
         if next_page_token:
             params["page"] = next_page_token
         if self.replication_key:
             params["sort"] = "asc"
             params["order_by"] = self.replication_key
         return params
+
+    def _prepare_query_search(self) -> dict:
+        """Return a dictionary with query params"""
+        query_search = {}
+
+        config_params = {
+            "days_to_decrease_custom_data": "ref_date:start",
+            "days_to_decrease_tasks": "updated_at:start",
+            "days_to_decrease_kpis": "ref_date:start",
+        }
+
+        if self.days_to_decrease_config_name:
+            params = config_params[self.days_to_decrease_config_name]
+            query_search[params] = str(
+                date.today()
+                - timedelta(
+                    days=int(self.config.get(self.days_to_decrease_config_name))
+                )
+            )
+            query_search["limit"] = 1000
+        return query_search
--- tap_sensedata/streams.py
+++ tap_sensedata/streams.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-import os
-from datetime import date, timedelta, datetime
 
 from tap_sensedata.client import sensedataStream
 
@@ -15,18 +13,16 @@ class CustomersStream(sensedataStream):
     schema_filepath = SCHEMAS_DIR / "customers.json"
     records_jsonpath = "$.{}[*]".format(name)
 
+
 class CustomDataStream(sensedataStream):
     name = "custom_data"
     path = "/custom_data"
     primary_keys = ["id"]
-    ref_date_start = date.today() - timedelta(days=int(os.getenv("TAP_SENSEDATA_CUSTOM_DATA_REF_DATE_START")))
-    query_search={
-        "ref_date:start": f'{ref_date_start}',
-        "limit": 1000
-    }
+    days_to_decrease_config_name = "days_to_decrease_custom_data"
     replication_key = "ref_date"
     schema_filepath = SCHEMAS_DIR / "custom_data.json"
     records_jsonpath = "$.{}[*]".format(name)
+
 
 class ContractsStream(sensedataStream):
     name = "contracts"
@@ -36,12 +32,14 @@ class ContractsStream(sensedataStream):
     schema_filepath = SCHEMAS_DIR / "contracts.json"
     records_jsonpath = "$.{}[*]".format(name)
 
+
 class ContractsStatusStream(sensedataStream):
     name = "contracts_status"
     path = "/contracts_status"
     primary_keys = ["id"]
     schema_filepath = SCHEMAS_DIR / "contracts_status.json"
     records_jsonpath = "$.{}[*]".format(name)
+
 
 class TasksTypesStream(sensedataStream):
     name = "tasks_types"
@@ -50,6 +48,7 @@ class TasksTypesStream(sensedataStream):
     schema_filepath = SCHEMAS_DIR / "tasks_types.json"
     records_jsonpath = "$.{}[*]".format(name)
 
+
 class TasksStatusStream(sensedataStream):
     name = "tasks_status"
     path = "/tasks_status"
@@ -57,18 +56,16 @@ class TasksStatusStream(sensedataStream):
     schema_filepath = SCHEMAS_DIR / "tasks_status.json"
     records_jsonpath = "$.{}[*]".format(name)
 
+
 class TasksStream(sensedataStream):
     name = "tasks"
     path = "/tasks"
     primary_keys = ["id"]
     replication_key = "updated_at"
-    updated_at_start = datetime.today() - timedelta(days=int(os.getenv("TAP_SENSEDATA_TASKS_UPDATED_AT_START")))
-    query_search={
-        "updated_at:start": f'{updated_at_start}',
-        "limit": 1000
-    }
+    days_to_decrease_config_name = "days_to_decrease_tasks"
     schema_filepath = SCHEMAS_DIR / "tasks.json"
     records_jsonpath = "$.{}[*]".format(name)
+
 
 class PlaybooksStream(sensedataStream):
     name = "playbooks"
@@ -78,15 +75,12 @@ class PlaybooksStream(sensedataStream):
     schema_filepath = SCHEMAS_DIR / "playbooks.json"
     records_jsonpath = "$.{}[*]".format(name)
 
+
 class KpisStream(sensedataStream):
     name = "kpis"
     path = "/kpis"
     primary_keys = ["type.name", "ref_date", "id_customer"]
-    ref_date_start = date.today() - timedelta(days=int(os.getenv("TAP_SENSEDATA_KPIS_REF_DATE_START")))
-    query_search={
-        "ref_date:start": f'{ref_date_start}',
-        "limit": 1000
-    }
+    days_to_decrease_config_name = "days_to_decrease_kpis"
     replication_key = "ref_date"
     schema_filepath = SCHEMAS_DIR / "kpis.json"
     records_jsonpath = "$.{}[*]".format(name)
