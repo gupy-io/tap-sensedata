8ec564a25f1f8934ebec1fa28340ac70f9edcf78
Fernandesfelipe1989 <fernandes.felipe1989@gmail.com>
2023-10-30 21:09:41 -0300

    fix(ContactsSchema): fix the indentation of the contacts schema.

--- tap_sensedata/schemas/contacts.py
+++ tap_sensedata/schemas/contacts.py
@@ -2,24 +2,25 @@ from singer_sdk import typing as th
 
 schema = th.PropertiesList(
     th.Property("id", th.IntegerType),
-    th.Property("customer",
-                th.ObjectType(
-                    th.Property("id_legacy", th.StringType),
-                    th.Property("name", th.StringType),
-                    th.Property("name_contract", th.StringType),
-                )
-                ),
+    th.Property(
+        "customer",
+        th.ObjectType(
+            th.Property("id_legacy", th.StringType),
+            th.Property("name", th.StringType),
+            th.Property("name_contract", th.StringType),
+        ),
+    ),
     th.Property("is_active", th.BooleanType),
     th.Property("name", th.StringType),
     th.Property("email", th.EmailType),
-    th.Property("custom_fields",
-                th.ObjectType(
-                    th.Property("comunicacoes_cm",
-                                th.ObjectType(
-                                    th.Property("value", th.StringType)
-                                ),
-                                ),
-                    ),
-                required=False,
-                ),
+    th.Property(
+        "custom_fields",
+        th.ObjectType(
+            th.Property(
+                "comunicacoes_cm",
+                th.ObjectType(th.Property("value", th.StringType)),
+            ),
+        ),
+        required=False,
+    ),
 ).to_dict()
