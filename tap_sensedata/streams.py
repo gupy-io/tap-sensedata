23a774365298933948a793d3762ee4146cef7f3d
Marcos Wesley <marcoswesley314@gmail.com>
2023-11-28 14:33:09 -0300

    chore(dags): remove replication_key to extract full (#14)
    * chore(dags): remove replication_key to extract full
    
    * chore(tap-sensedata): change replication_key to updated_at

--- tap_sensedata/streams.py
+++ tap_sensedata/streams.py
@@ -89,7 +89,7 @@ class KpisStream(sensedataStream):
 class ActiveContactsStream(sensedataStream):
     name = "contacts"
     path = "/contacts"
+    replication_key = "updated_at"
     primary_keys = ["id"]
-    replication_key = "created_at"
     schema_filepath = SCHEMAS_DIR / "contacts.json"
     records_jsonpath = "$.{}[*]".format(name)
